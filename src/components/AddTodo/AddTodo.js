64d147ca3fd5f42380271652688fd889846fbdd1
Antoine Rawlins <twanrawlins@outlook.com>
2020-02-12 01:20:02 -0600

    More styling and color changes done to components

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -5,14 +5,11 @@ import classes from './AddTodo.module.css'
 export default function AddTodo(props) {
     return (
       <Form className={classes.AddTodo} onSubmit={props.addTodo} >
-           <InputGroup>
-        <FormControl placeholder='Enter Task' value={props.todo} onChange={props.getTodo}/>
-          {/* <input type='text' value={props.todo} onChange={props.getTodo}/>
-          <input type='submit' value='Submit' /> */}
-       
+        <InputGroup>
+          <FormControl placeholder='Enter Task' value={props.todo} onChange={props.getTodo}/>
             <InputGroup.Append >
-              <Button variant='outline-secondary' type='reset' onClick={props.clearInput}>Clear</Button>
-              <Button variant='outline-secondary' type='submit' >Add Task</Button>
+              <Button variant='danger btn-sm' type='reset' onClick={props.clearInput}>Clear</Button>
+              <Button variant='success btn-sm' type='submit' >Add Task</Button>
             </InputGroup.Append>
         </InputGroup>
       </Form>
