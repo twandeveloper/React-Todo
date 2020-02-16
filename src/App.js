6d7880fdd9741c373fb5f4a5872792db4120abe3
Antoine Rawlins <twanrawlins@outlook.com>
2020-02-15 22:18:30 -0600

    app is linked to firebase database to hold todo list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
+import axios from './axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Todos from './components/Todos/Todos'
-
 import Header from './components/Header/Header'
 
 import './App.css'
@@ -10,10 +10,36 @@ import './App.css'
 
 export class App extends Component {
   state = {
-    todos: [
-      {task: 'todo 1'}
-    ],
+    todos: [],
     todo: ''
+  }
+
+  componentDidMount() {
+    axios.get('/todos.json')
+      .then(res => {
+        const fireTodo  = res.data
+        console.log(res.data);
+        
+        const todoArr = []
+
+        for(let key in fireTodo) {
+          todoArr.push(fireTodo[key])
+        }
+        console.log(todoArr);
+        this.setState({todos: todoArr})
+      })
+
+      
+      
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.todos === this.state.todos) {
+      return false
+    }
+    console.log('fired off');
+    this.setState({todos: nextState.todos})
+    return true
   }
 
   getTodoHandler = (event) => {
@@ -23,16 +49,21 @@ export class App extends Component {
   }
 
   addTodoHandler = (event, taskIndex) => { 
-    const todoObj = {task: this.state.todo}
-    console.log(todoObj);
-    console.log(taskIndex);
+
+    const newTodo = {
+      task: this.state.todo,
+      user: 'Antoine'
+    }
+
+    axios.post('/todos.json', newTodo)
+      .then(response => {
+        console.log(response);
+        const updateTodos = [...this.state.todos, newTodo]
+        this.setState({todos: updateTodos})
+        this.setState({todo: ''})
+      })
+
     
-    
-    const todos = [...this.state.todos, todoObj]
-    console.log(todos);
-    
-    this.setState({todos: todos})
-    this.setState({todo: ''})
     event.preventDefault()
     event.target.reset()
   }
@@ -45,7 +76,8 @@ export class App extends Component {
   deleteTodo = (taskIndex) => {
     const todos = [...this.state.todos]
     todos.splice(taskIndex, 1)
-    this.setState({todos: todos}) 
+    this.setState({todos: todos})
+
   }
 
   render() {
